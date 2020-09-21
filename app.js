7442ae2b7ec8f855ed4ab7d0e0b3d237ee66fadb
manasrajowar66 <58780138+manasrajowar66@users.noreply.github.com>
2020-09-21 20:55:47 +0530

    cncrypt password

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ const userSchema = new mongoose.Schema({
   password:String
 });
 
-const secret = "tumhochutiye";
-userSchema.plugin(encrypt,{secret:secret,encryptedFields: ["password"]});
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields: ["password"]});
 const User = mongoose.model("User",userSchema);
 
 app.get("/",function(req,res){
